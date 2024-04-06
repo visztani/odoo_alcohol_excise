0d686ad7b0ebc3beedfc0c5aba069e8972c3e014
visztani <visztani@gmail.com>
2024-04-06 02:16:11 +0200

    a

--- models/product_template.py
+++ models/product_template.py
@@ -5,8 +5,12 @@ class ProductTemplate(models.Model):
 
     excise_active = fields.Boolean('Track Excise',default=False)
     excise_abv = fields.Float('ABV',help='Average By Volume (% Alcohol')
+    excise_hlf = fields.Float('HLF',help='Hectolitre Percent Alcohol by Volume')
     excise_category = fields.Many2one('excise.category','Excise Category')
     excise_volume = fields.Float('Excisable Volume (L)', help='Volume for the basis of the Excise calculation')
+    excise_knkod = fields.Char('KN Code',help='Közös Nómenklatúra (Common Nomenclature) Code')
+    excise_fajtakod = fields.Char('Fajtakód',help='Fajtakód')
+    excise_guarantee_nedded = fields.Boolean('Guarantee Needed',default=True)
     
 class ProductProduct(models.Model):
     _inherit = 'product.product'
