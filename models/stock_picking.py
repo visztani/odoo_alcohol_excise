ca05819c0adcfb63b18bf74523457377029c8ddb
visztani <visztani@gmail.com>
2024-04-12 19:12:46 +0200

    a

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -6,9 +6,10 @@ class StockPicking(models.Model):
     excise_picking_hlf = fields.Float(string='HLF Total', compute='_compute_picking_line_hlf', store=True)
     excise_adomennyiseg_kod = fields.Char(string='Adóm. kód', related='sale_id.excise_adomennyiseg_kod', readonly=True)
     JOGCIMKODOK = [('32203','KI Jöv. kisker - 32203'),('20503','BE tagállamból adófelfügesztéssel - 32203'),('32201','KI Magánszemély - 32201'),('32202','KI Jöv. eng. nagyker - 32202'),('0','Nem jövedéki')]
-    excise_jogcimkod = fields.Selection(string='Jogcímkód', related='sale_id.excise_jogcimkod', readonly=True)
+    excise_jogcimkod = fields.Selection(string='Jogcímkód', related='sale_id.excise_jogcimkod', readonly=True, store=True)
     #BEKULDESSTATUSZOKJR = [('0','Nem beküldött'), ('1','Beküldött'), ('2','Javított'), ('3','Rontottnak jelölt')]
     #jovedeki_raktar_NAV_fele_bekuldott_e = fields.Selection(string='NAV felé elküldve?', selection=BEKULDESSTATUSZOKJR)
+    excise_car_no = fields.Char('Rendszám', related = 'sale_id.excise_car_no', readonly=True, store=True)
 
     @api.depends('move_ids_without_package', 'move_ids_without_package.total_hlf') # type: ignore
     def _compute_picking_line_hlf(self):
--- models/stock_warehouse.py
+++ models/stock_warehouse.py
@@ -5,6 +5,7 @@ class Warehouse(models.Model):
     _inherit = 'stock.warehouse'
 
     excise_warehouse_no = fields.Char('Excise Warehouse No.', help='number issued by tax authority to suspend excise liablity')
+    excise_car_number = fields.Char('Rendszám')
 
 class Location(models.Model):
     _inherit = 'stock.location'
@@ -12,7 +13,6 @@ class Location(models.Model):
     excise_unpaid = fields.Boolean('Duty Unpaid',help='Location stores stock wtihout excise', compute='_compute_excise_unpaid')
     excise_paid_manual = fields.Boolean('Duty Paid location', help='Location is a duty paid location within a duty unpaid warehouse')
     excise_warehouse_no = fields.Char('Excise Warehouse No.', help='number issued by tax authority to suspend excise liablity', compute='_compute_whseno')
-
 
     @api.depends('excise_paid_manual')
     def _compute_excise_unpaid(self):
