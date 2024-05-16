42c20112d9cc21a10a8ffed0a73ec1ba09f362a5
visztani <visztani@gmail.com>
2024-05-17 01:31:14 +0200

    a

--- models/__init__.py
+++ models/__init__.py
@@ -11,12 +11,12 @@ from . import stock_move
 from . import excise_move
 from . import stock_move_line
 
-_logger.info('ExciseMove model loaded.')
+#_logger.info('ExciseMove model loaded.')
 
-@api.model
-def create(self, vals):
-    _logger.info('Creating excise.move record with values: %s', vals)
-    return super(excise_move, self).create(vals)
+#@api.model
+#def create(self, vals):
+#    _logger.info('Creating excise.move record with values: %s', vals)
+#    return super(excise_move, self).create(vals)
 
 #def unlink(self, vals):
 #    _logger.info('deleting excise.move record with values: %s', vals)
