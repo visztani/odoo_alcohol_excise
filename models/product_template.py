a3bfc2ab89ca496657a675b4babf019cdbc4cb11
visztani <visztani@gmail.com>
2024-05-18 17:54:47 +0200

    a

--- models/product_template.py
+++ models/product_template.py
@@ -4,13 +4,18 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     excise_active = fields.Boolean('Track Excise',default=False)
-    excise_abv = fields.Float('ABV',help='Average By Volume (% Alcohol')
+    excise_abv = fields.Float('ABV',help='Alcohol By Volume (% Alcohol')
     excise_hlf = fields.Float('HLF',help='Hectolitre Percent Alcohol by Volume')
     excise_category = fields.Many2one('excise.category','Excise Category')
     excise_volume = fields.Float('Excisable Volume (L)', help='Volume for the basis of the Excise calculation')
     excise_knkod = fields.Char('KN Code',help='Közös Nómenklatúra (Common Nomenclature) Code')
     excise_fajtakod = fields.Char('Fajtakód',help='Fajtakód')
     excise_guarantee_needed = fields.Boolean('Guarantee Needed',default=False,help='Excise Guarantee Needed')
+    excise_guarantee_needed = fields.Selection([
+        ('0', 'Biztosítékmentes'), ('1', 'Biztosítékköteles'),
+        ('3', 'Adózott jöv. termék'),
+        ('4', 'Nem jöv. term.')], string='Stock Type',
+        index=True)
     
 class ProductProduct(models.Model):
     _inherit = 'product.product'
