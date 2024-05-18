58ac4b0d389a9265d3558695277ff3ab3dcf554b
visztani <visztani@gmail.com>
2024-05-18 17:07:49 +0200

    a

--- models/excise_move.py
+++ models/excise_move.py
@@ -73,7 +73,7 @@ class excise_move(models.Model):
                 record.move_source_address = record.stock_move_id.company_id.partner_id.id
             else:
                 _logger.info('Source location is external for partner ID %s', record.stock_move_id.picking_id.partner_id.id)
-                record.move_source_address = record.stock_move_id.picking_id.partnerid.id
+                record.move_source_address = record.stock_move_id.picking_id.partner_id.id
             
             if record.move_location_dest_id.usage == 'internal':
                 _logger.info('Destination location is internal for partner ID %s', record.stock_move_id.company_id.partner_id.id)
