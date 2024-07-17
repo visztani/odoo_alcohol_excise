2a79bd82edf52de104d2d7a2a5dd2f6768b06cad
visztani <visztani@gmail.com>
2024-07-17 22:11:07 +0200

    a

--- models/sale_order.py
+++ models/sale_order.py
@@ -4,9 +4,9 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     excise_sale_order_hlf = fields.Float(string='HLF Total', compute='_compute_order_line_hlf', store=True)
-    excise_adomennyiseg_kod = fields.Char(string='Adóm. kód')
+    excise_adomennyiseg_kod = fields.Char(string='Adóm. kód', required = True)
     JOGCIMKODOK = [('32203','KI Jöv. kisker - 32203'),('20503','BE tagállamból adófelfügesztéssel - 32203'),('32201','KI Magánszemély - 32201'),('32202','KI Jöv. eng. nagyker - 32202'),('0','Nem jövedéki')]
-    excise_jogcimkod = fields.Selection(string='Jogcímkód', selection=JOGCIMKODOK)
+    excise_jogcimkod = fields.Selection(string='Jogcímkód', selection=JOGCIMKODOK, reqired = True)
     #BEKULDESSTATUSZOKJR = [('0','Nem beküldött'), ('1','Beküldött'), ('2','Javított'), ('3','Rontottnak jelölt')]
     #jovedeki_raktar_NAV_fele_bekuldott_e = fields.Selection(string='NAV felé elküldve?', selection=BEKULDESSTATUSZOKJR)
     excise_car_no = fields.Char('Rendszám', default='kocsi1')
