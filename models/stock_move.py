b8c1f6622927cec8491d8b984919169a676bc06f
visztani <visztani@gmail.com>
2024-07-25 18:00:00 +0200

    a

--- models/stock_move.py
+++ models/stock_move.py
@@ -94,12 +94,12 @@ class StockMove(models.Model):
         index=True,
         readonly=True)
         JOGCIMKODOK = [('32203','KI Jöv. kisker - 32203'),('20503','BE tagállamból adófelfügesztéssel - 32203'),('32201','KI Magánszemély - 32201'),('32202','KI Jöv. eng. nagyker - 32202'),('0','Nem jövedéki')]
-        excise_move_jogcimkod = fields.Selection(string='Jojgcímkód', selection=JOGCIMKODOK, compute='_compute_excise_move_jogcimkod', store=True, readonly=False)
+        excise_move_line_jogcimkod = fields.Selection(string='Jojgcímkód', selection=JOGCIMKODOK, compute='_compute_excise_move_line_jogcimkod', store=True, readonly=False)
 
-    @api.depends('excise_move_jogcimkod')
-    def _compute_excise_move_jogcimkod(self):
+    @api.depends('excise_move_line_jogcimkod')
+    def _compute_excise_move_line_jogcimkod(self):
         for line in self:
-            line.excise_move_jogcimkod = line.move_id.excise_line_jogcimkod
+            line.excise_move_line_jogcimkod = line.move_id.excise_line_jogcimkod
     
         @api.depends('product_id')
         def _compute_excise_stock_type(self):
