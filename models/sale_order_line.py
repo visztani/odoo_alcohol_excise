83ec756119ac287532866090c76e2f5a0cbb05f7
visztani <visztani@gmail.com>
2025-04-18 14:39:25 +0200

    a

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -4,7 +4,7 @@ class SaleOrderLine(models.Model):
 
     _inherit = 'sale.order.line'
 
-    excise_sale_order_line_biztositek = fields.Boolean(string='Biztosíték?', related='product_id.excise_guarantee_needed', readonly=True)
+    #excise_sale_order_line_biztositek = fields.Boolean(string='Biztosíték?', related='product_id.excise_guarantee_needed', readonly=True)
     excise_line_fajtakod = fields.Char(string='Fajtakód', related='product_id.excise_fajtakod', readonly=True)
     excise_line_knkod = fields.Char(string='KN kód', related='product_id.excise_knkod', readonly=True)
     total_hlf = fields.Float(string='HLF Line', compute='_compute_total_hlf', store=True)
