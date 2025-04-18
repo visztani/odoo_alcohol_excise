8ee5c31f31e764f089eb3164d1d82e72b1c9d723
visztani <visztani@gmail.com>
2025-04-18 14:17:36 +0200

    a

--- models/stock_move.py
+++ models/stock_move.py
@@ -25,7 +25,6 @@ class StockMove(models.Model):
         index=True,
         readonly=True)
     
-    
     @api.depends('location_id.excise_stock_type')
     def _compute_excise_stock_type(self):
         for record in self:
@@ -33,6 +32,18 @@ class StockMove(models.Model):
                 record.excise_move_excise_stock_type = record.location_id.excise_stock_type
             else:
                 record.excise_move_excise_stock_type = False
+
+    '''
+    @api.depends('product_id')
+    def _compute_excise_stock_type(self):
+        for record in self:
+            if record.product_id:
+                #_logger.info('EM Computing excise stock type for product ID %s', record.product_id.excise_stock_type)
+                #record.excise_move_excise_stock_type = record.product_id.excise_stock_type
+                pass
+            else:
+                _logger.info('EM No product associated with this excise move record')
+    '''
 
     '''
     @api.depends('product_id')
